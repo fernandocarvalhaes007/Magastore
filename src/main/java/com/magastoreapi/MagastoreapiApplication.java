dcdd2c9b8746617202af6080d735ea67566c9c45
fernandosantos07@gmail.com <fernandosantos07@gmail.com>
2024-07-16 12:46:30 -0300

    iniciando o projeto MAGASTORE - API

---
+++ src/main/java/com/magastoreapi/MagastoreapiApplication.java
@@ -0,0 +1,14 @@
+package com.magastoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagastoreapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagastoreapiApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/magastoreapi/magastoreapi/MagastoreapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magastoreapi.magastoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagastoreapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
